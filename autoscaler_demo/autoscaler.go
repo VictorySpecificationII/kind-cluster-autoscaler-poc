3e059df010d3242642edc57bda42695ea960a242
Antreas Christofi <a.christofi@cyi.ac.cy>
2025-10-02 20:23:52 +0300

    observe worker node cpu load logic implemented

--- autoscaler_demo/autoscaler.go
+++ autoscaler_demo/autoscaler.go
@@ -90,8 +90,40 @@ func get_worker_nodes() map[string]string{
 
 }
 
-func observe_cluster_load_on_available_workers() {
+func observe_cluster_load_on_available_workers() map[string]string {
+	worker_node_loads := make(map[string]string)
+	cmd := exec.Command("kubectl", "top", "nodes", "-l", "role=worker")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error running kubectl: ", err)
+		fmt.Println("Output: ", string(output))
+		return nil
+	}
 
+	lines := strings.Split(string(output), "\n")
+//	fmt.Println(lines) //debug
+
+        for _, line := range lines[1:] {
+                if line == "" {
+                        continue
+                }
+
+        parts := strings.Fields(line)
+//      fmt.Println(parts) //debug
+
+        node := parts[0]
+        load := parts[2]
+
+//      fmt.Println(status) //debug
+        if strings.Contains(load, "%"){
+        worker_node_loads[node] = load
+        }
+
+//	fmt.Println(worker_node_loads) //debug
+
+	}
+
+	return worker_node_loads
 }
 
 //func autoscale_pods_on_available_workers() {
@@ -99,8 +131,10 @@ func observe_cluster_load_on_available_workers() {
 //}
 
 func main(){
-	cordoned_nodes := get_cordoned_off_nodes()
-	fmt.Println(cordoned_nodes)
-	worker_nodes := get_worker_nodes()
-	fmt.Println(worker_nodes)
+//	cordoned_nodes := get_cordoned_off_nodes()
+//	fmt.Println(cordoned_nodes)
+//	worker_nodes := get_worker_nodes()
+//	fmt.Println(worker_nodes)
+	load := observe_cluster_load_on_available_workers()
+	fmt.Println(load)
 }
